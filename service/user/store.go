be7622c2c856301776a14ecf803b56f7cb8a007e
AdvenAdam <advenadam@student.uns.ac.id>
2024-12-16 16:30:55 +0700

    adding db Migration & compliting user store function

--- service/user/store.go
+++ service/user/store.go
@@ -38,14 +38,37 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
 }
 
 func (s *Store) CreateUser(user *types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (first_name, last_name, email, password, created_at) VALUES (?, ?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password, user.CreatedAt)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (s *Store) UpdateUser(user *types.User) error {
+	_, err := s.db.Exec("UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?", user.FirstName, user.LastName, user.Email, user.Password, user.ID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
